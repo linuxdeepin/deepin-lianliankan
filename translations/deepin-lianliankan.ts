ed331adf00298fbfa918abce4d3422b70ab72ed1
feeengli <fengli@uniontech.com>
2022-09-14 16:23:28 +0800

    chore: update translations
    update translations
    
    Log: update translations

--- translations/deepin-lianliankan.ts
+++ translations/deepin-lianliankan.ts
@@ -4,17 +4,17 @@
 <context>
     <name>CloseWindowDialog</name>
     <message>
-        <location filename="../src/source/closewindowdialog.cpp" line="47"/>
+        <location filename="../src/source/closewindowdialog.cpp" line="32"/>
         <source>Are you sure you want to exit the game?</source>
         <translation>Are you sure you want to exit the game?</translation>
     </message>
     <message>
-        <location filename="../src/source/closewindowdialog.cpp" line="55"/>
+        <location filename="../src/source/closewindowdialog.cpp" line="40"/>
         <source>Keep Playing</source>
         <translation>Keep Playing</translation>
     </message>
     <message>
-        <location filename="../src/source/closewindowdialog.cpp" line="56"/>
+        <location filename="../src/source/closewindowdialog.cpp" line="41"/>
         <source>Exit</source>
         <translation>Exit</translation>
     </message>
@@ -22,12 +22,12 @@
 <context>
     <name>GameoverBlurEffectWidget</name>
     <message>
-        <location filename="../src/source/gameoverblureffectwidget.cpp" line="105"/>
+        <location filename="../src/source/gameoverblureffectwidget.cpp" line="90"/>
         <source>Play Again</source>
         <translation>Play Again</translation>
     </message>
     <message>
-        <location filename="../src/source/gameoverblureffectwidget.cpp" line="106"/>
+        <location filename="../src/source/gameoverblureffectwidget.cpp" line="91"/>
         <source>Have a Rest</source>
         <translation>Have a Rest</translation>
     </message>
@@ -35,14 +35,14 @@
 <context>
     <name>Main</name>
     <message>
-        <location filename="../src/main.cpp" line="63"/>
-        <location filename="../src/main.cpp" line="65"/>
-        <location filename="../src/main.cpp" line="66"/>
+        <location filename="../src/main.cpp" line="48"/>
+        <location filename="../src/main.cpp" line="50"/>
+        <location filename="../src/main.cpp" line="51"/>
         <source>LianLianKan</source>
         <translation>LianLianKan</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="64"/>
+        <location filename="../src/main.cpp" line="49"/>
         <source>Lianliankan is a puzzle game.</source>
         <translation>Lianliankan is a puzzle game.</translation>
     </message>
@@ -50,22 +50,22 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../src/source/mainpage.cpp" line="45"/>
+        <location filename="../src/source/mainpage.cpp" line="30"/>
         <source>Easy</source>
         <translation>Easy</translation>
     </message>
     <message>
-        <location filename="../src/source/mainpage.cpp" line="46"/>
+        <location filename="../src/source/mainpage.cpp" line="31"/>
         <source>Normal</source>
         <translation>Normal</translation>
     </message>
     <message>
-        <location filename="../src/source/mainpage.cpp" line="47"/>
+        <location filename="../src/source/mainpage.cpp" line="32"/>
         <source>Hard</source>
         <translation>Hard</translation>
     </message>
     <message>
-        <location filename="../src/source/mainpage.cpp" line="65"/>
+        <location filename="../src/source/mainpage.cpp" line="50"/>
         <source>Select Level</source>
         <translation>Select Level</translation>
     </message>
@@ -73,12 +73,12 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/source/mainwindow.cpp" line="108"/>
+        <location filename="../src/source/mainwindow.cpp" line="92"/>
         <source>FAIL</source>
         <translation>FAIL</translation>
     </message>
     <message>
-        <location filename="../src/source/mainwindow.cpp" line="111"/>
+        <location filename="../src/source/mainwindow.cpp" line="95"/>
         <source>VICTORY</source>
         <translation>VICTORY</translation>
     </message>
--- translations/deepin-lianliankan_fr.ts
+++ translations/deepin-lianliankan_fr.ts
@@ -1,20 +1,20 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="pl">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="fr">
 <context>
     <name>CloseWindowDialog</name>
     <message>
         <location filename="../src/source/closewindowdialog.cpp" line="47"/>
         <source>Are you sure you want to exit the game?</source>
-        <translation>Czy na pewno chcesz wyjść z gry?</translation>
+        <translation>Voulez-vous vraiment quitter le jeu ?</translation>
     </message>
     <message>
         <location filename="../src/source/closewindowdialog.cpp" line="55"/>
         <source>Keep Playing</source>
-        <translation>Graj dalej</translation>
+        <translation>Continuer à jouer</translation>
     </message>
     <message>
         <location filename="../src/source/closewindowdialog.cpp" line="56"/>
         <source>Exit</source>
-        <translation>Wyjście</translation>
+        <translation>Quitter</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../src/source/gameoverblureffectwidget.cpp" line="105"/>
         <source>Play Again</source>
-        <translation>Zagraj ponownie</translation>
+        <translation>Rejouer</translation>
     </message>
     <message>
         <location filename="../src/source/gameoverblureffectwidget.cpp" line="106"/>
         <source>Have a Rest</source>
-        <translation>Odpocznij</translation>
+        <translation>Se reposer</translation>
     </message>
 </context>
 <context>
@@ -42,7 +42,7 @@
     <message>
         <location filename="../src/main.cpp" line="64"/>
         <source>Lianliankan is a puzzle game.</source>
-        <translation>LianLianKan jest grą logiczną</translation>
+        <translation>Lianliankan est un jeu de réflexion.</translation>
     </message>
 </context>
 <context>
@@ -50,22 +50,22 @@
     <message>
         <location filename="../src/source/mainpage.cpp" line="45"/>
         <source>Easy</source>
-        <translation>Łatwy</translation>
+        <translation>Facile</translation>
     </message>
     <message>
         <location filename="../src/source/mainpage.cpp" line="46"/>
         <source>Normal</source>
-        <translation>Normalny</translation>
+        <translation>Normal</translation>
     </message>
     <message>
         <location filename="../src/source/mainpage.cpp" line="47"/>
         <source>Hard</source>
-        <translation>Trudny</translation>
+        <translation>Difficile</translation>
     </message>
     <message>
         <location filename="../src/source/mainpage.cpp" line="65"/>
         <source>Select Level</source>
-        <translation>Wybierz poziom</translation>
+        <translation>Choisir le niveau</translation>
     </message>
 </context>
 <context>
@@ -73,12 +73,12 @@
     <message>
         <location filename="../src/source/mainwindow.cpp" line="108"/>
         <source>FAIL</source>
-        <translation>PORAŻKA</translation>
+        <translation>PERDU</translation>
     </message>
     <message>
         <location filename="../src/source/mainwindow.cpp" line="111"/>
         <source>VICTORY</source>
-        <translation>ZWYCIĘSTWO</translation>
+        <translation>VICTOIRE</translation>
     </message>
 </context>
 </TS>
