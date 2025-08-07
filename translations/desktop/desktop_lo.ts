7416bd826f65d1b549578a464ee7628cc7445e52
Tian Shilin <tianshilin@uniontech.com>
2025-08-07 16:28:55 +0800

    feat: Update translations for Lao locale
    log: Translation update

---
+++ translations/desktop/desktop_lo.ts
@@ -0,0 +1,16 @@
+<?xml version="1.0" ?><!DOCTYPE TS>
+<TS language="lo" version="2.1">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>LianLianKan</source>
+        <translation>ລຽນລຽນກັນ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>LianLianKan</source>
+        <translation>ເກມປິດສະໄຫມ</translation>
+    </message>
+</context>
+</TS>
